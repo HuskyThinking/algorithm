5cb900e452c90f86d8e5b546f258bf10bacb36b4
HuskyThinking <849627763@qq.com>
2022-09-25 16:27:39 +0800

    新增035

---
+++ array/src/main/java/husky/base/LeetCode035.java
@@ -0,0 +1,15 @@
+package husky.base;
+
+/**
+ * @ClassName: LeetCode035
+ * @Description: 035. 搜索插入位置
+ * @Author: Fisher
+ * @Date: 2022/9/25 16:25
+ **/
+public class LeetCode035 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
