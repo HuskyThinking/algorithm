02409bf60b668361bc097cd7839126d1afb59c01
dingxin.li <849627763@qq.com>
2022-10-28 16:08:53 +0800

    015、018

---
+++ LeetCode/src/main/java/net/husky/hash/LeetCode015.java
@@ -0,0 +1,16 @@
+package net.husky.hash;
+
+/**
+ * @author: dingxin.li
+ * @date: 2022/10/18 14:56
+ * @description: 15. 三数之和
+ */
+public class LeetCode015 {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+    }
+
+
+
+}
---
+++ LeetCode/src/main/java/net/husky/hash/LeetCode018.java
@@ -0,0 +1,14 @@
+package net.husky.hash;
+
+/**
+ * @author: dingxin.li
+ * @date: 2022/10/18 15:02
+ * @description: 18. 四数之和
+ */
+public class LeetCode018 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
